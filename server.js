ffffbeef32515e9cbd55e351a06711f64ac7ca06
joe44850 <jlinstrum@dropbox.com>
2018-01-28 15:43:26 -0800

    Modified mongoose connection

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ else{
   var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongoose.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
